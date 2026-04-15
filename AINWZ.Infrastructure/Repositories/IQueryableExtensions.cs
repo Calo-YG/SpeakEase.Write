d1d60716b5d73392b597f8d9d7f47822b6f283fb
wyg <3164522206@qq.com>
2026-04-16 02:13:33 +0800

    添加ef 查询扩展类

---
+++ AINWZ.Infrastructure/Repositories/IQueryableExtensions.cs
@@ -0,0 +1,50 @@
+﻿using AINWZ.Infrastructure.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AINWZ.Infrastructure.Repositories
+{
+    public static class IQueryableExtensions
+    {
+        /// <summary>
+        /// whereif
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="predicate"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+
+        /// <summary>
+        /// 分页
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="pagination"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static PageResult<T> ToPageResult<T>(this IQueryable<T> source, Pagination pagination)
+        {
+            var total = source.Count();
+            var data = source.Skip((pagination.Page - 1) * pagination.PageSize).Take(pagination.PageSize).ToList();
+            return PageResult<T>.Create(total, data);
+        }
+
+        /// <summary>
+        /// 异步分页
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="pagination"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static async Task<PageResult<T>> ToPageResultAsync<T>(this IQueryable<T> source, Pagination pagination)
+        {
+            var total = await source.CountAsync();
+            var data = await source.Skip((pagination.Page - 1) * pagination.PageSize).Take(pagination.PageSize).ToListAsync();
+            return PageResult<T>.Create(total, data);
+        }
+    }
+}
